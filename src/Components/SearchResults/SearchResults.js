7d961033cefdefcd69ee6df26410865fce00365a
Carlos Fabián <carl.fab.g@gmail.com>
2020-04-25 15:53:51 -0500

    se creo SearchResults component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,15 @@
+import react, { Component } from 'react'
+import './SearchResults.css'
+
+class SearchResults extends Component {
+    render(){
+        return(
+            <div className="SearchResults">
+            <h2>Results</h2>
+            
+            </div>
+        )
+    }
+}
+
+export default SearchResults
