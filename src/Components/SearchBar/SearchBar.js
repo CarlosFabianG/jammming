a50d00e40f3bee734e80b730e389c20b31c0075a
Carlos Fabián <carl.fab.g@gmail.com>
2020-05-02 23:34:29 -0500

    sea creo metodo handleTermChange en searchBar.js y se agrego atributo onChange a la etiqueta input dentro de metodo render

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,16 +3,24 @@ import './SearchBar.css'
 
 
 class SearchBar extends Component {
+    state = {
+        term: null
+    }
+
+    handleTermChange = (e) => {
+        const changeTerm = e.target.value
+        console.log(changeTerm)
+        this.setState({term: changeTerm})
+    }
 
     search = () => {
         this.props.onSearch(this.state.term)
     }
 
-
     render(){
         return(
          <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
+            <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
             <button className="SearchButton">SEARCH</button>
         </div>
         )
