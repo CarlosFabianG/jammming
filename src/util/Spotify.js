fb18ea56351f570e72769f7e50a35adb05f8b7d0
Carlos Fabián <carl.fab.g@gmail.com>
2020-05-03 23:15:33 -0500

    se agrego metodo en Spotify.js

---
+++ src/util/Spotify.js
@@ -0,0 +1,19 @@
+const token
+const clientId
+const redirectURI
+
+const Spotify = {
+
+    getAccessToken = () => {
+        if(toke){
+            return token
+        }else{
+
+        }
+    },
+    search = (term) => {
+
+    }
+}
+
+export default Spotify
